af92ce45452ef6c2b9722f1d36fdb90a39b6f251
Kasun24 <kasun961224@gmail.com>
2023-11-21 13:37:05 +0530

    new page templates

---
+++ wp-content/themes/wirefox/page-news.php
@@ -0,0 +1,59 @@
+<?php
+/* 
+Template Name: News List
+*/
+get_header();
+?>
+<div class="inner-content">
+		<div class="two-blocks padding-align">
+			<div class="text-col">
+				<div>
+					<h1>DIGITAL MARKETING NEWS</h1>
+					<p>Winning Approach to Web Design - Stand out online and generate prospects</p>
+				</div>			
+			</div>
+			<div class="image-col">
+				<img src="https://wirefox.co.uk/wp-content/uploads/2021/06/Wirefox-our-work.jpg" alt="" width="" height=""/>
+			</div>
+		</div><!--two blocks end-->
+	
+			<div class="news-breadcrumb padding-align">
+				<ul>
+					<li><a href="#">SEO</a><span>/</span></li>
+					<li><a href="#">WEB DESIGN</a><span>/</span></li>
+					<li><a href="#">WEB DEVELOPMENT</a><span>/</span></li>
+					<li><a href="#">INTERNET</a><span>/</span></li>
+					<li><a href="#">SOCIAL MEDIA</a></li>
+				</ul>		
+			</div>
+
+	
+	
+				<div class="news-list-row padding-align">					
+					<div class="news-image">
+						<img src="https://wirefox.co.uk/wp-content/uploads/2022/11/2022_Hand-met-sterren_Kwaliteti_S_1621075879.jpg" alt="" width="" height=""/>
+						<h3 class="news-headings">What Is White Label Reputation Management ?</h3>
+					</div>			
+					<div class="news-date">
+						<span>9 November 2022</span>					
+					</div>					
+				</div><!--news list section end-->	
+	
+				<div class="news-list-row padding-align">					
+					<div class="news-image">
+						<img src="https://wirefox.co.uk/wp-content/uploads/2022/11/2022_Hand-met-sterren_Kwaliteti_S_1621075879.jpg" alt="" width="" height=""/>
+						<h3 class="news-headings">What Is White Label Reputation Management ?</h3>
+					</div>			
+					<div class="news-date">
+						<span>9 November 2022</span>					
+					</div>					
+				</div><!--news list section end-->	
+		
+		
+</div><!--inner content end-->
+
+
+
+
+<?php
+get_footer();
