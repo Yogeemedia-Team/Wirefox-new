918d7d24584f501dbed27f673ff3d08b0f6c3c87
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-11-21 23:33:53 +0530

    banner section button

--- wp-content/themes/wirefox/blocks/inner_banner.php
+++ wp-content/themes/wirefox/blocks/inner_banner.php
@@ -3,6 +3,18 @@
             <div class="py-5 p-md-0 pe-md-5 mt-4 mt-md-0">
                 <h1 class="title"><span><?php the_field('main_title');?></span> <?php the_field('main_title_weight_text');?></h1>
                 <p><?php the_field('banner_paragrph');?></p>
+                <div class="button-wrap right large-btn has-animation animated" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px); opacity: 1;">
+                    <div class="icon-wrap parallax-wrap" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px);">
+                        <div class="button-icon parallax-element" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px);">
+                            <i class="arrow-icon-down"></i>
+                        </div>
+                    </div>
+                    <a target="_blank" href="<?php the_sub_field('button_link'); ?>">
+                        <div class="button-text sticky right">
+                            <span data-hover="<?php the_sub_field('button_text'); ?>"><?php the_sub_field('button_text'); ?></span>
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
         
@@ -14,4 +26,5 @@
                 <?php endif; ?>
            
         </div>
+        
 </div>
