3ba6df286f700c56ff92d8d8b1e2fa1876137a8e
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-11-22 01:44:52 +0530

    modification kasun styles

--- wp-content/themes/wirefox/blocks/inner_banner.php
+++ wp-content/themes/wirefox/blocks/inner_banner.php
@@ -4,7 +4,7 @@
                 <h1 class="title"><span><?php the_field('main_title');?></span> <?php the_field('main_title_weight_text');?></h1>
                 <p><?php the_field('banner_paragrph');?></p>
             <?php if( get_field('button_text') ): ?>
-                <div class="button-wrap right large-btn has-animation animated" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px); opacity: 1;">
+                <div class="button-wrap right large-btn has-animation animated inner-banner-btn" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px); opacity: 1;">
                                 <div class="icon-wrap parallax-wrap" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px);">
                                     <div class="button-icon parallax-element" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px);">
                                         <i class="arrow-icon-down"></i>
