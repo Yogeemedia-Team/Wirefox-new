61e132d900c56d3eced7df4c7d3b8d185c70bcce
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-11-21 23:37:35 +0530

    banner section button

--- wp-content/themes/wirefox/blocks/inner_banner.php
+++ wp-content/themes/wirefox/blocks/inner_banner.php
@@ -3,7 +3,8 @@
             <div class="py-5 p-md-0 pe-md-5 mt-4 mt-md-0">
                 <h1 class="title"><span><?php the_field('main_title');?></span> <?php the_field('main_title_weight_text');?></h1>
                 <p><?php the_field('banner_paragrph');?></p>
-                <div class="button-wrap right large-btn has-animation animated" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px); opacity: 1;">
+<?php if( get_field('button_text') ): ?>
+    <div class="button-wrap right large-btn has-animation animated" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px); opacity: 1;">
                     <div class="icon-wrap parallax-wrap" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px);">
                         <div class="button-icon parallax-element" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px);">
                             <i class="arrow-icon-down"></i>
@@ -15,6 +16,9 @@
                         </div>
                     </a>
                 </div>
+<?php endif; ?>
+                
+
             </div>
         </div>
         
