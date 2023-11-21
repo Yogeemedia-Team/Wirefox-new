5c705f47dd44f2e1ba49b34d5c76714fab419282
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-11-22 00:09:46 +0530

    banner section button

--- wp-content/themes/wirefox/blocks/latest-post-section.php
+++ wp-content/themes/wirefox/blocks/latest-post-section.php
@@ -4,12 +4,40 @@
             <p><?php the_field('section_content');?></p>
         </div>
         <div id="news-row">
-            <img src="https://wirefox.co.uk/wp-content/uploads/2022/11/2022_Hand-met-sterren_Kwaliteti_S_1621075879.jpg" alt="" width="" height="" />
-            <div class="news-col">
-                <span>9 November 2022</span>
-                <h3>
-                    What Is White Label Reputation Management ?
-                </h3>
-            </div>
+            <?php
+                // The Query
+                $args = array(
+                    'post_type' => 'post', // You can change this to 'page' or any other custom post type
+                    'posts_per_page' => 1,
+                    'orderby'        => 'date', // Order by date
+                    'order'          => 'DESC', // Number of posts to display
+                );
+
+                $query = new WP_Query($args);
+
+                // The Loop
+                if ($query->have_posts()) {
+                    while ($query->have_posts()) {
+                        $query->the_post();
+                        $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'thumbnail');
+                        ?>
+                        <img src="<?php echo $thumbnail_url;?>" alt="" width="" height="" />
+                            <div class="news-col">
+                                <span><?php echo get_the_date('j F Y'); ?></span>
+                                <h3>
+                                    <?php the_title(); ?>
+                                </h3>
+                            </div>
+                        <?php
+                    }
+                    /* Restore original Post Data */
+                    wp_reset_postdata();
+                } else {
+                    // No posts found
+                    echo 'No posts found';
+                }
+                ?>
+
+            
         </div>
     </div><!--news section end-->
