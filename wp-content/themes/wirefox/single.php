e54eb8019a0324900d61bfdec49838d68f124b2e
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-11-24 13:51:05 +0530

    single page

--- wp-content/themes/wirefox/single.php
+++ wp-content/themes/wirefox/single.php
@@ -11,36 +11,28 @@ get_header();
 
 
 
+<div class="inner-content about-us-content">
 <?php if (have_posts()) : while (have_posts()): the_post() ; ?>
-<div class="row main_banner row_padding_left row_padding_right row_padding_top row_padding_bottom dark-section change-header-color">
-	<div class="col-md-7 my-auto">
-		<div class="py-5 p-md-0 pe-md-5 mt-4 mt-md-0">
-			<h1 class="title"><?php the_title();?></h1>
-		</div>
-	</div>
-	<div class="col-md-5">
-		<div class="animated-img">
-			<img src="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full');?>" />
-		</div>
-	</div>
-	<div class="news_cont row_padding_top">
-		<div class="row">
-			<div class="col-md-9">
-				<?php the_content();?>
-			</div>
-			<div class="col-md-3">
-				<!-- post sidebar(if need) -->
-			</div>
-		</div>
-	</div>
+<div class="row padding-align main_banner change-header-color row_padding_top row_padding_bottom">
+    <div class="col-md-7 my-auto">
+        <div class="py-5 p-md-0 pe-md-5 mt-4 mt-md-0">
+            <h1 class="title"><?php the_title();?></h1> 
+        </div>
+    </div>
+
+    <div class="col-md-5">
+        <div class="animated-img">
+                <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full');?>"/>
+        </div>
+    </div>
+
 </div>
+<?php the_content()?>
 <?php endwhile; else: ?>
 			<p><?php _e('Sorry, no records matched your criteria.'); ?></p>
 	<?php endif; ?>
 
-
-
-
+</div>
 <?php
 get_footer();
 ?>
