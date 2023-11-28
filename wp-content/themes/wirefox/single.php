c38353f28401c7963cbb9c08025304dd266e1480
Kasun24 <kasun961224@gmail.com>
2023-11-28 15:18:55 +0530

    task-36

--- wp-content/themes/wirefox/single.php
+++ wp-content/themes/wirefox/single.php
@@ -28,7 +28,7 @@ get_header();
         </div>
         <div class="news_cont">
             <div class="row">
-                <div class="col-md-9">
+                <div class="col-md-10">
                     <?php the_content() ?>
                 </div>
 
@@ -38,11 +38,122 @@ get_header();
             <p><?php _e('Sorry, no records matched your criteria.'); ?></p>
         <?php endif; ?>
 
-        <div class="col-md-3">
-            <!-- post sidebar(if need) -->
+        <div class="col-md-2">
+            <!-- share icons -->
+            <div class="sh_icons row_padding_top row_padding_bottom">
+                <ul class="m-0">
+                    <li>
+                        <a href="">
+                            <span class="icon_box">
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="icon_box">
+                                <i class="fa-brands fa-twitter"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="icon_box">
+                                <i class="fa-brands fa-linkedin-in"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="icon_box">
+                                <i class="fa-brands fa-facebook-messenger"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="icon_box">
+                                <i class="fa-brands fa-whatsapp"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="icon_box">
+                                <i class="fa-regular fa-envelope"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="icon_box">
+                                <i class="fa-brands fa-reddit-alien"></i>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
             </div>
         </div>
+        <!-- feature post section -->
+        <div class="row_padding_top row_padding_bottom row_padding_left row_padding_right">
+            <div class="posts_sec">
+                <h3 class="mb-5">Further Reading</h3>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="post_author mb-4 text-left">
+                            <span><img decoding="async" class="auth_img" src="http://localhost/Wirefox-new/wp-content/themes/wirefox/assets/images/user_icon.jpeg" alt=""></span>
+                            <span> <a class="auth_name" href="">adm-wirefox</a></span>
+                            <span class="">
+                                <div class="line"></div>
+                            </span>
+                            <span class="text-light">1616-0808-2121
+                            </span>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="post_img">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Wirefox-Digital-Agency-Birmingham.jpg" alt="Image Title">
+                                </div>
+                            </div>
+                            <div class="col-lg-8 my-auto">
+                                <div class="w-75">
+                                    <h5 class="mb-0"><a href="https://wirefox.co.uk/ecommerce-marketing-strategies-to-increase-sales/" class="link">Ecommerce Marketing Strategies to Increase sales</a></h5>
+                                    <i class="fa-solid fa-2x fa-plus"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="post_author mb-4 text-left">
+                            <span><img decoding="async" class="auth_img" src="http://localhost/Wirefox-new/wp-content/themes/wirefox/assets/images/user_icon.jpeg" alt=""></span>
+                            <span> <a class="auth_name" href="">adm-wirefox</a></span>
+                            <span class="">
+                                <div class="line"></div>
+                            </span>
+                            <span class="text-light">1616-0808-2121
+                            </span>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="post_img">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Wirefox-Digital-Agency-Birmingham.jpg" alt="Image Title">
+                                </div>
+                            </div>
+                            <div class="col-lg-8 my-auto">
+                                <div class="w-75">
+                                    <h5 class="mb-0"><a href="https://wirefox.co.uk/ecommerce-marketing-strategies-to-increase-sales/" class="link">Ecommerce Marketing Strategies to Increase sales</a></h5>
+                                    <i class="fa-solid fa-2x fa-plus"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- feature post section -->
+
         <?php
         get_footer();
         ?>
