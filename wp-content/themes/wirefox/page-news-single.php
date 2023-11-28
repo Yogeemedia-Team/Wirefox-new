2a96dcbd021989368f27264413f777f1cf791a0a
Kasun24 <kasun961224@gmail.com>
2023-11-28 10:51:51 +0530

    gitignore

--- wp-content/themes/wirefox/page-news-single.php
+++ wp-content/themes/wirefox/page-news-single.php
@@ -17,27 +17,28 @@ get_header();
 			<img src="https://wirefox.co.uk/wp-content/uploads/2021/06/Wirefox-our-work.jpg" />
 		</div>
 	</div>
-	<div class="news_cont row_padding_top">
-		<div class="row">
-			<div class="col-md-9">
-				<p>
-					White-label reputation management solutions are software and services that can be rebranded to fit a business' needs when it comes to monitor their online reputation and reviews. The software, usually in the form of a platform, would be a product that can be rebranded, while services like review response and review generation would be a white-label fulfillment option. You could combine these together for a comprehensive reputation management solution by finding a provider that offers both reputable management products and services.
-				</p>
+</div>
+<div class="news_cont row_padding_top">
+	<div class="row">
+		<div class="col-md-9">
+			<p>
+				White-label reputation management solutions are software and services that can be rebranded to fit a business' needs when it comes to monitor their online reputation and reviews. The software, usually in the form of a platform, would be a product that can be rebranded, while services like review response and review generation would be a white-label fulfillment option. You could combine these together for a comprehensive reputation management solution by finding a provider that offers both reputable management products and services.
+			</p>
 
-				<h3>
-					What is Reputation Management?
-				</h3>
+			<h3>
+				What is Reputation Management?
+			</h3>
 
-				<p>
-					Though reputation management might not be a new concept, it has definitely seen some changes over the years - particularly with the dawn of the internet. In today's world, reputation is better defined by a star rating on Google than by word-of-mouth referrals. This is because the internet has brought us all a little closer together and made it easier for people to connect with one another and share their experiences with businesses.
-				</p>
-			</div>
-			<div class="col-md-3">
-				<!-- post sidebar(if need) -->
-			</div>
+			<p>
+				Though reputation management might not be a new concept, it has definitely seen some changes over the years - particularly with the dawn of the internet. In today's world, reputation is better defined by a star rating on Google than by word-of-mouth referrals. This is because the internet has brought us all a little closer together and made it easier for people to connect with one another and share their experiences with businesses.
+			</p>
+		</div>
+		<div class="col-md-3">
+			<!-- post sidebar(if need) -->
 		</div>
 	</div>
 </div>
+
 
 
 
